5dfb289535a2ed408ce74fcab4d60604f0b5611e
Amoghsaraswat <75422601+Amoghsaraswat@users.noreply.github.com>
2021-09-15 16:52:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ function setup() {
 function draw() {
     image(img,0,0,600,420);
     fill("red");
-    text("dog",30,40);
+    text("dog",60,70);
     noFill();
     stroke("red");
-    rect(50,60,620,420)
+    rect(50,60,450,350);
 }
